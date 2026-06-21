596d708e4a3965705e30e449ddf43b18fd3c6fe1
sandra97-cloud <sndralxa97@gmail.com>
2026-06-22 00:01:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const text =
+"Welcome 👋";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,100);
+
+}
+
+}
+
+typing();
